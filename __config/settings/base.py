fefb72ed2fd733764ff71263e3481ddd2a54ab02
Dylan Jenkinson <dylan@dylan-jenkinson.nz>
2017-01-30 06:52:15 +1300

    Let logging coexist to try to get wsgi logs.

--- __config/settings/base.py
+++ __config/settings/base.py
@@ -234,7 +234,7 @@ RAVEN_CONFIG = {
 
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': True,
+    'disable_existing_loggers': False,
     'root': {
         'level': 'WARNING',
         'handlers': ['sentry'],
